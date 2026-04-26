fa05cd638528e6d377d31ce36eb8b5eb4247b7bb
Your Name <your_email@example.com>
2026-04-26 16:57:56 +0530

    made search not case sensitive and poster update

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ API_BASE="http://www.omdbapi.com/"
 if not API_KEY:
     raise RuntimeError("API_KEY missing.put it in .env as API_KEY=xxxx")
 
+print(API_KEY)
 
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
@@ -21,23 +22,37 @@ df = pickle.load(open(os.path.join(base_dir, "df.pkl"), "rb"))
 tfidf_matrix = pickle.load(open(os.path.join(base_dir, "tfidf_matrix.pkl"), "rb"))
 indices = pickle.load(open(os.path.join(base_dir, "indices.pkl"), "rb"))
 
+indices = {k.lower(): v for k, v in indices.items()}
 
 def fetch_poster(title):
     try:
-        url = f"{API_BASE}?t={title}&apikey={API_KEY}"
+        url = f"http://www.omdbapi.com/?t={title}&apikey={API_KEY}"
         data = requests.get(url).json()
-        return data.get("Poster", "https://via.placeholder.com/300x450?text=No+Image")
+
+        poster = data.get("Poster")
+
+        if poster and poster != "N/A":
+            return poster
+        else:
+            return "https://via.placeholder.com/300x450?text=No+Image"
+
     except:
-        return "https://via.placeholder.com/300x450?text=No+Image"
+        return "https://via.placeholder.com/300x450?text=Error"
 
 
 def recommend(title):
     title = title.lower()
+    #matches = [movie for movie in indices.keys() if title in movie]
 
-    if title not in indices:
+    matches = [k for k in indices.keys() if title in k]
+
+    if not matches:
         return [], []
 
-    idx = indices[title]
+    idx = indices[matches[0]]
+
+    #closest_title = matches[0]
+    #idx = indices[closest_title]
 
     sim_scores = list(enumerate(cosine_similarity(tfidf_matrix[idx], tfidf_matrix)[0]))
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)[1:11]
